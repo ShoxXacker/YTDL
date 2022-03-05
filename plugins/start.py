0ad6177c5e069236d052369e0a9ee011305212fb
ShoxXacker <67458072+ShoxXacker@users.noreply.github.com>
2022-03-06 01:33:25 +0500

    Create start.py

---
+++ plugins/start.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@Client.on_message(Filters.command(["start"]), group=-2)
+async def start(client, message):
+    # return
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Support Channel", url="https://t.me/InFoJosTel")],
+        [InlineKeyboardButton(
+            "Support Group", url="https://t.me/InFoJosTelGroup")]
+    ])
+    welcomed = f"Hi! <b>{message.from_user.first_name}</b>\n/help lo thawn la aw, Min hmandan tur i hriat duh chuan."
+    await message.reply_text(welcomed, reply_markup=joinButton)
+    raise StopPropagation
